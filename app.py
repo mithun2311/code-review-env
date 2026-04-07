5b3c77d970fd8b043aab8c7c393cc5b34edeedc1
Hruday P N <hrudaypn2005@gmail.com>
2026-04-07 20:30:01 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from env.env import CodeReviewEnv
+from env.models import Action
+
+app = FastAPI()
+
+env = CodeReviewEnv()
+
+@app.post("/reset")
+def reset():
+    obs = env.reset()
+    return obs.dict()
+
+@app.post("/step")
+def step(action: dict):
+    act = Action(**action)
+    obs, reward, done, info = env.step(act)
+    return {
+        "observation": obs.dict(),
+        "reward": reward,
+        "done": done,
+        "info": info
+    }
+
+@app.get("/")
+def root():
+    return {"status": "ok"}
